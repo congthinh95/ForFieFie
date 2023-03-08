92c52b77b06b7bead9b0a6389781576b822bb35e
Tran Cong Thinh (MS/EDA12-XC) <Thinh.TranCong@vn.bosch.com>
2023-03-08 15:20:48 +0700

    thinh dep trai

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle:'Yoooooo !',
+    introDesc: `Trên thiên đường có 10 thiên thần,
+    5 thiên thần đang chơi đùa,
+    4 thiên thần đang nói chuyện,
+    à 1 thiên thần đang đọc tin nhắn chúc mừng ngày 8/3 này.`,
+    btnIntro: 'hihi',
+    title: 'Thiệp 8/3 dành tặng riêng cho Fie FIe',
+    desc: 'hmmmmmmmmmmmmmmmmmm !',
+    btnYes: 'Vuiiiii :)) ',
+    btnNo: 'không thíchhhhhh ',
+    question:'Biết ngay là sẽ thích màaaaaaaaaaaaaa <3',
+    btnReply: 'Gửi cho bạn',
+    reply: 'Fie rất thích cám ơn Thịnh nho =)))',
+    mess: 'Tớ biết mà. ',
+    messDesc: 'Chúc Fie Fie một ngày 8/3 vui vẻ và tràn ngập hạnh phúc nhé ',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.youtube.com/watch?v=Q9C2i_Z2ETA' 
+}
